ca23401579982d54ec471c7432c1e00fde6f0a25
ericktorres <ericktorresdeveloper@gmail.com>
2018-11-24 23:02:13 -0600

    Improved login page

--- src/App.js
+++ src/App.js
@@ -3,25 +3,42 @@ import './App.css';
 import Signinform from './signinform';
 
 class App extends Component {
+
+  state = {
+    password: "username",
+    email: "email",
+    authentication: false
+  }
+
+  authenticate = (valid) => {
+    this.setState({
+      authentication: true
+    })
+  }
+
+  changeUserInfo = (event) => {
+
+    console.log(event.target.value)
+  }
+
   render() {
     return (
       <div className="App">
       <div className="navbar">
-      <h1 className="company-name">Database Project</h1>
+      <h1 className="company-name">Tesla Automobile Sales</h1>
       </div>
       <div className="row forms">
 
         <div className="col-sm-6">
 
-          <Signinform formTitle="Employee Sign-In"></Signinform>
+          <Signinform  changeinfo = {this.changeUserInfo} formTitle="Employee Sign-In"></Signinform>
 
         </div>
 
         <div className="col-sm-6">
-
-          <Signinform formTitle="Customer Sign-In"></Signinform>
-
+          <Signinform  changeinfo = {this.changeUserInfo} formTitle="Customer Sign-In"></Signinform>
         </div>
+
       </div>
       <div className="row extra-spacing"></div>
       </div>
--- src/signinform.js
+++ src/signinform.js
@@ -5,24 +5,37 @@ class Signinform extends Component {
     render() {
         return (
             <form className="signform">
-            <div className="container">
-            <h3 className="text-center">{this.props.formTitle}</h3>
+                <div className="container">
+                <h3 className="text-center">{this.props.formTitle}</h3>
 
-              <div className="row username">
-              <label for="uname">Email</label>
-              <input className="text-field" type="text" placeholder="Enter Username" name="uname" required></input>
-  
-              </div>
-              
-              <div className="row password">
-              <label for="pass">Password</label>
-              <input className="text-field" type="text" placeholder="Enter Password" name="pass" required></input>
-              </div>
+                <div className="row">
+                    <div className="col-sm-8 offset-sm-2 black_bottom"></div>
+                </div>
 
-              <input className="submission" type="submit" value="Sign-In" />
-              <input className="submission" type="submit" value="Sign-Up" />
+                {/* Email Submission */}
+                <div className="col-sm-8 offset-sm-2">
+                    <div className="row email">
+                        <label>Email
+                        <input className="text-field" type="text"  placeholder="Enter Email" name="email" required></input>
+                        </label>
+                    </div>
+                </div>
 
-            </div>
+                {/* Password Submission */}
+                <div className="col-sm-8 offset-sm-2">
+                    <div className="row password">
+                        <label>Password
+                        <input className="text-field" type="password"  placeholder="Enter Password" name="pass" required></input>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="col-sm-6 offset-sm-3">
+                <input className="buttons" type="submit" value="Sign-In" />
+                </div>
+                {/* <input className="buttons" type="submit" value="Sign-Up" /> */}
+
+                </div>
           </form>
         )
     }
