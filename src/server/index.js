3d3bb1e6aecf27598bc3460e7a9813dc1b123254
ericktorres <ericktorresdeveloper@gmail.com>
2018-12-01 19:39:08 -0600

    Changed index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const functions = require('./functions');
+const functions = require('./functions');
 
 
 
@@ -29,6 +29,11 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+
+app.post('/signup', function(req, res) {
+
+
 });
 
 //Here we are handling the login request
@@ -65,6 +70,6 @@ app.post('/stock', function(req, res) {
 
 
 //This is where we will be listening to the requests
-app.listen(3002, () => {
+app.listen(3001, () => {
     console.log('Products server listening on port 3001')
 })
